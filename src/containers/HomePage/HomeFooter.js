c503cdf47fe77348722d93ec97c982ed99a3b800
phanvanviet0209 <entiviet@gmail.com>
2023-03-15 01:10:36 +0700

    #53

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+class HomeFooter extends Component {
+    render() {
+        return (
+            <div className='home-footer'>
+                <p>&copy; 2023 Phan Van Viet. More Infomation, please visit my facebook office.<a target='_blank' href='https://www.facebook.com/profile.php?id=100063100556943'> Click here</a></p>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => { //tác dụng hàm này là để truyền đc kiểu this.props
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
